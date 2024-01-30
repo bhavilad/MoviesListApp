189bb7b527448a762ffcfbea6874619fd6b9e220
Pinkal Mistry <pinkal@cafu.com>
2024-01-30 16:26:28 +0400

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Pinkal Mistry on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
